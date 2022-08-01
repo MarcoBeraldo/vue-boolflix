e36d664c168e0ed84ef9ed9eb6a0b5cd7710a707
MarcoBeraldo <beraldomar@gmail.com>
2022-08-01 16:41:22 +0200

    moved apis to app

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,47 @@
 <template>
-  <div id="app">
+  <div id="app" @clicked-search="getMovies">
     <AppHeader />
   </div>
 </template>
 
 <script>
+import axios from "axios";
 import AppHeader from "/src/components/AppHeader.vue";
 export default {
   name: "App",
   components: {
     AppHeader,
+  },
+  data() {
+    return {
+      query: "",
+      movies: [],
+      series: [],
+    };
+  },
+  methods: {
+    getMovies() {
+      this.fetchMovie();
+      this.fetchSerie();
+    },
+    fetchMovie() {
+      axios
+        .get(
+          `https://api.themoviedb.org/3/search/movie?api_key=aedbe765e1515ff23693adb543bba89d&query=${this.query}&language=it-IT`
+        )
+        .then((res) => {
+          this.movies = res.data.results;
+        });
+    },
+    fetchSerie() {
+      axios
+        .get(
+          `https://api.themoviedb.org/3/search/tv?api_key=aedbe765e1515ff23693adb543bba89d&query=${this.query}&language=it-IT`
+        )
+        .then((res) => {
+          this.series = res.data.results;
+        });
+    },
   },
 };
 </script>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,43 +1,19 @@
 <template>
   <header>
     <input v-model="query" />
-    <button @click="getMovies">Cerca</button>
+
+    <button @click="emitClick">Cerca</button>
   </header>
 </template>
 
 <script>
-import axios from "axios";
 export default {
   name: "AppHeader",
-  data() {
-    return {
-      query: "",
-      movies: [],
-      series: [],
-    };
-  },
+
+  components: {},
   methods: {
-    getMovies() {
-      this.fetchMovie();
-      this.fetchSerie();
-    },
-    fetchMovie() {
-      axios
-        .get(
-          `https://api.themoviedb.org/3/search/movie?api_key=aedbe765e1515ff23693adb543bba89d&query=${this.query}&language=it-IT`
-        )
-        .then((res) => {
-          this.movies = res.data.results;
-        });
-    },
-    fetchSerie() {
-      axios
-        .get(
-          `https://api.themoviedb.org/3/search/tv?api_key=aedbe765e1515ff23693adb543bba89d&query=${this.query}&language=it-IT`
-        )
-        .then((res) => {
-          this.series = res.data.results;
-        });
+    emitClick() {
+      this.$emit("clicked-search");
     },
   },
 };
