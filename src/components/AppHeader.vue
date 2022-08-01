c82482084322abafe0812c2ef1a36259adff1d7f
MarcoBeraldo <beraldomar@gmail.com>
2022-08-01 15:40:59 +0200

    added api requests

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,21 +6,37 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "AppHeader",
   data() {
     return {
       query: "",
+      movies: [],
+      series: [],
     };
   },
   methods: {
     getMovies() {
+      this.fetchMovie();
+      this.fetchSerie();
+    },
+    fetchMovie() {
       axios
         .get(
-          `https://api.themoviedb.org/3/search/movie?api_key=aedbe765e1515ff23693adb543bba89d&query=${query}&language=it-IT`
+          `https://api.themoviedb.org/3/search/movie?api_key=aedbe765e1515ff23693adb543bba89d&query=${this.query}&language=it-IT`
         )
         .then((res) => {
           this.movies = res.data.results;
+        });
+    },
+    fetchSerie() {
+      axios
+        .get(
+          `https://api.themoviedb.org/3/search/tv?api_key=aedbe765e1515ff23693adb543bba89d&query=${this.query}&language=it-IT`
+        )
+        .then((res) => {
+          this.series = res.data.results;
         });
     },
   },
